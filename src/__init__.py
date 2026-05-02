a743de5b99130e90b3dff115fff784884d9d4075
Prathima Kadari <kadariprathima4@gmail.com>
2026-05-02 20:28:31 +0530

    Created __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from config.config import config_dict
+
+
+def create_app(env="development"):
+    """
+    Flask application factory
+    """
+    app = Flask(__name__)
+
+    # Load configuration
+    app.config.from_object(config_dict[env])
+
+    # Register Blueprints
+    from .routes import main
+    app.register_blueprint(main)
+
+    return app
